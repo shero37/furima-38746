e5da81bd2d50787a54c5bc322cccd73fb9d8f279
shero37 <ushimookani3712@gmail.com>
2022-12-25 18:05:02 +0900

    アソシエーションとバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  validates :image, :item_name, :description, :price, presence: true
+  validates :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end

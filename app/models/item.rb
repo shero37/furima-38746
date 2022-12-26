f7c1f9b78949e8737a2878eac0d3007a2b68b83b
shero37 <ushimookani3712@gmail.com>
2022-12-26 16:32:02 +0900

    itemモデルのバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
 
   validates :image, :item_name, :description, presence: true
   validates :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
-  with_options presence: true, format: { with: /\A[0-9]+\z/, message: "is out of setting range" } do
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  end
+  validates :price, presence: true
+  validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range" }
 end

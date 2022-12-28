d79c70ea8c918d02817863657994e78cd2e9dc92
shero37 <ushimookani3712@gmail.com>
2022-12-28 20:32:27 +0900

    Formオブジェクトパターンの実装

---
+++ app/models/order_destination.rb
@@ -0,0 +1,17 @@
+class クラス名
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :order_id, :user_id, :item_id
+
+  with_options presence: true do
+    validates :phone_number, numericality: {only_integer: true, message: 'is invalid. Input only number'}
+    validates :municipality, :address, :order_id, :user_id, :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :phone_number, length: { minimum: 10, maximum: 11 }
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end

509496db6d9f1e9d66c5ee88d9a015c7fd6d1545
shero37 <ushimookani3712@gmail.com>
2023-01-09 17:53:47 +0900

    エラーメッセージの日本語化

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -3,11 +3,11 @@ class OrderDestination
   attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number' }
+    validates :phone_number, numericality: { only_integer: true, message: 'が正しくありません  数字を入力してください' }
     validates :municipality, :address, :user_id, :item_id, :token
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'が正しくありません  ハイフンを含んで入力してください' }
   end
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "を入力してください" }
   validates :phone_number, length: { minimum: 10, maximum: 11 }
 
   def save
